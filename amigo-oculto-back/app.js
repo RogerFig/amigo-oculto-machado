7bf5994ec5df4fdffd2bc4df0c27e6d3a81a23a1
Rogerio Figueredo <rfigsousa@gmail.com>
2023-11-13 09:17:04 -0300

    Projeto em branco para desenvolvimento

--- amigo-oculto-back/app.js
+++ amigo-oculto-back/app.js
@@ -1,14 +1,42 @@
 const express = require("express");
-const rotas = require("./api");
-const cors = require("cors");
 
 const PORT = 3000;
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 
-app.use("/", rotas);
+lista_pessoas = [
+  {
+    id: "1",
+    nome: "Maria José",
+    telefone: "86999885544",
+  },
+  {
+    id: "2",
+    nome: "José Maria",
+    telefone: "86999885544",
+  },
+  {
+    id: "3",
+    nome: "Marta Maria",
+    telefone: "86999885544",
+  },
+  {
+    id: "4",
+    nome: "João Maria",
+    telefone: "86999885544",
+  },
+];
+
+let proxId = lista_pessoas.length + 1;
+
+app.get("/", (req, res) => {});
+
+app.post("/", (req, res) => {});
+
+app.delete("/:id", (req, res) => {});
+
+app.put("/:id", (req, res) => {});
 
 app.listen(PORT, () => console.log(`Aplicação rodando na porta ${PORT}`));
--- amigo-oculto-front/js/util.js
+++
@@ -1,9 +0,0 @@
-const botaoAdicionar = document.querySelector("#in-user");
-
-botaoAdicionar.addEventListener("click", (e) => {
-  const formUser = document.querySelector("#formUser");
-
-  formUser.inNome.value = "";
-  formUser.inTelefone.value = "";
-  formUser.idHidden.value = "";
-});
